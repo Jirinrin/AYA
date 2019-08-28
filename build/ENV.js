729af5848e5bbafa0a7abd880b5823eef31ee36b
Jirinrin <jiri.ei@hotmail.com>
2019-08-28 20:12:35 +0200

    gitignore build folder

--- build/ENV.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Editable
-exports.default = {
-    folder: undefined,
-    recursionDepth: 3,
-};
--- build/types/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var IterationType;
-(function (IterationType) {
-    IterationType[IterationType["shallow"] = 0] = "shallow";
-    IterationType[IterationType["deep"] = 1] = "deep";
-})(IterationType = exports.IterationType || (exports.IterationType = {}));
-;
