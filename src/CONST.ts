9df4b87a6ead1293b6412000570c69e94d5fd4d6
Jirinrin <jiri.ei@hotmail.com>
2020-05-02 16:55:03 +0200

    Write CONSTs down a bit more robustly

--- src/CONST.ts
+++ src/CONST.ts
@@ -1,5 +1,13 @@
-// Not editable
-export default Object.freeze({
+interface IConst {
+  musicMetadata: boolean,
+  imageMetadata: boolean,
+}
+
+const Const: IConst = {
   musicMetadata: false,
   imageMetadata: true,
-});
+}
+
+// Not editable
+const C = Object.freeze(Const);
+export default C;
