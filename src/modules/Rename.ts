174f7be2d5ff7c7836d8969bd08ddbfdfe37f147
Jirinrin <jiri.ei@hotmail.com>
2020-05-02 19:56:56 +0200

    Remove rename methods I've never used

--- src/modules/Rename.ts
+++ src/modules/Rename.ts
@@ -63,36 +63,36 @@ const module: RawFactoryModule = {
       })
   }),
 
-  everyEntryHasToMatch: (iterator: FileIteratorInitFunction) => ({
-    abbrev: 'eehtm',
-    help: 'for every entry in folder rename to {$2: string} if it matches {$1: regex}',
-    /**
-     * @param exp: thing to try to match
-     * @param put: thing to put before the fileName
-     */
-    run: (exp: RegExp, put: string) => {
-      iterator((folder, ent) => {
-        if (!ent.name.toLowerCase().match(exp)) {
-          renameFile(folder, ent.name, `${put} - ${ent}`);
-        }
-      });
-    }
-  }),
+  // everyEntryHasToMatch: (iterator: FileIteratorInitFunction) => ({
+  //   abbrev: 'eehtm',
+  //   help: 'for every entry in folder rename to {$2: string} if it matches {$1: regex}',
+  //   /**
+  //    * @param exp: thing to try to match
+  //    * @param put: thing to put before the fileName
+  //    */
+  //   run: (exp: RegExp, put: string) => {
+  //     iterator((folder, ent) => {
+  //       if (!ent.name.toLowerCase().match(exp)) {
+  //         renameFile(folder, ent.name, `${put} - ${ent}`);
+  //       }
+  //     });
+  //   }
+  // }),
 
-  everyEntryHasToInclude: (iterator: FileIteratorInitFunction) => ({
-    abbrev: 'eehti',
-    help: 'for every entry in folder rename if it includes {$1: string} you provide',
-    /**
-     * @param part: thing that every filename has to include
-     */
-    run: (part: string) => {
-      iterator((folder, ent) => {
-        if (!ent.name.toLowerCase().includes(part.toLowerCase())) {
-          renameFile(folder, ent.name, `${part} - ${ent}`)
-        }
-      });
-    }
-  }),
+  // everyEntryHasToInclude: (iterator: FileIteratorInitFunction) => ({
+  //   abbrev: 'eehti',
+  //   help: 'for every entry in folder rename if it includes {$1: string} you provide',
+  //   /**
+  //    * @param part: thing that every filename has to include
+  //    */
+  //   run: (part: string) => {
+  //     iterator((folder, ent) => {
+  //       if (!ent.name.toLowerCase().includes(part.toLowerCase())) {
+  //         renameFile(folder, ent.name, `${part} - ${ent}`)
+  //       }
+  //     });
+  //   }
+  // }),
 }
 
 export default module;
