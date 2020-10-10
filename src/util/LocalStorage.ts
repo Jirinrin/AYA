b9cc14df378d770a18b30444458f7ff48ea33634
Jiri Swen <jiri.ei@hotmail.com>
2020-10-10 13:42:39 +0200

    Make config check if init keys exist in config.json

--- src/util/LocalStorage.ts
+++ src/util/LocalStorage.ts
@@ -1,31 +1,27 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-const defaultConfig = {
-  recursionDepth: 3,
-}
-type IConfig = typeof defaultConfig;
-
-type UserScript = string;
-type IUserScripts = Record<string, UserScript>;
-
-
 class LocalStorage<T extends Record<string, any>> {
   protected jsonPath: string;
   protected state: T;
 
   public get s() { return this.state };
 
-  constructor(fileName: string, initState: T) {
+  constructor(fileName: string, initState: T, checkAllKeys?: boolean) {
     const dir = path.resolve('./.ayaStorage');
     if (!fs.existsSync(dir))
       fs.mkdirSync(dir);
     
     this.jsonPath = path.resolve(dir, fileName);
     if (!fs.existsSync(this.jsonPath))
-      fs.writeFileSync(this.jsonPath, JSON.stringify(initState), 'utf8');
+      fs.writeFileSync(this.jsonPath, JSON.stringify(initState, null, 2), 'utf8');
 
     this.readState();
+
+    if (checkAllKeys)
+      Object.entries(initState).forEach(([k, v]: [keyof T, any]) => {
+        if (!this.state[k]) this.set(k, v);
+      })
   }
 
   protected readState() {
@@ -39,17 +35,23 @@ class LocalStorage<T extends Record<string, any>> {
   public set<K extends keyof T>(key: K, val: T[K]) {
     this.state[key] = val;
     this.writeState();
-    console.info(`Successfully set item with key "${key}"!`);
   }
 }
 
 
+const defaultConfig = {
+  recursionDepth: 3,
+}
+type IConfig = typeof defaultConfig;
 class Config extends LocalStorage<IConfig> {
   constructor() {
-    super('settings.json', defaultConfig);
+    super('config.json', defaultConfig, true);
   }
 }
 
+
+type UserScript = string;
+type IUserScripts = Record<string, UserScript>;
 class UserScripts extends LocalStorage<IUserScripts> {
   constructor() {
     super('scripts.json', {});
