770032af3174f457fdd8d6bced4fa284d37f4235
Jiri Swen <jiri.ei@hotmail.com>
2020-10-11 00:19:24 +0200

    Update highlight color scheme to dracula-like

--- src/highlightLookup.ts
+++ src/highlightLookup.ts
@@ -1,25 +1,34 @@
 import chalk, { Chalk } from "chalk";
 
-// todo: better color scheme (can use chalk.hex()?) https://github.com/dracula/prism
-// todo: include all kinds of regex related keywords: 'language-regex', 'regex-delimiter', 'quantifier', 'group', 'charclass'
+// Based on https://github.com/dracula/prism
+
+// const originalColors = {
+//   '--foreground': '#F8F8F2',
+//   '--comment':    '#6272A4',
+//   '--cyan':       '#8BE9FD',
+//   '--green':      '#50FA7B',
+//   '--orange':     '#FFB86C',
+//   '--pink':       '#FF79C6',
+//   '--purple':     '#BD93F9',
+//   '--red':        '#FF5555',
+//   '--yellow':     '#F1FA8C',
+//   not sure        '#e2777a',
+// }
 
 const highlights: [string[], Chalk][] = [
-  [["property", "tag", "boolean", "number", "constant", "symbol", "deleted", "regex-delimiter"], chalk.red], // #905
+  [['script', 'prolog', 'punctuation', 'charset'], chalk], // --foreground
+  [['comment', 'variable'], chalk.gray], // --comment
+  [['url', 'built-in', 'class-name', 'console', 'charset-punctuation'], chalk.cyan], // --cyan
+  [['atrule', 'attr-name', 'attr-value', 'function', 'method'], chalk.greenBright], // --green
+  [['property', 'parameter', 'group', 'symbol'], chalk.hex('#FFB86C')], // --orange
+  [['entity', 'keyword', 'important', 'selector', 'tag', 'operator', 'arrow', 'alternation'], chalk.magentaBright], // --pink
+  [['boolean', 'constant', 'number'], chalk.magenta], // --purple
+  [['regex', 'regex-delimiter', 'charset-negation'], chalk.red], // --red
+  [['string', 'char'], chalk.yellowBright], // --yellow
 
-  [["selector", "attr-name", "string", "char", "builtin", "inserted"], chalk.yellow], // #690
+  // [['inserted', 'property-access', 'quantifier', 'charclass', 'deleted', 'namespace'], chalk], // uncategorized
 
-  [["operator", "entity", "url", "string"], chalk.green], // #9a6e3a
-
-  [["atrule", "attr-value", "keyword"], chalk.blue], // #07a
-
-  [["function", "class-name"], chalk.cyan], // #DD4A68
-
-  [["regex", "important", "variable"], chalk.cyan], // #e90
-
-  [["punctuation"], chalk ], // #fff
-
-  [["italic"], chalk.italic],
-
+  [['italic'], chalk.italic],
   [["bold", "important"], chalk.bold],
 ];
 
