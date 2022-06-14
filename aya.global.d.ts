e1094f8499de70cf3a97b02aa8c31a0759d14e8b
Jirinrin <jiri.ei@hotmail.com>
2022-06-14 20:47:32 +0200

    Oops, actually make the aya global docs usable

--- aya.global.d.ts
+++ aya.global.d.ts
@@ -10,78 +10,72 @@ type ENV = any;
 type Lodash = any;
 type JSONBigInt = any;
 
-export type a = {}
+/**
+ * Execute a command that will be executed in the underlying shell environment.
+ */
+declare function exec(cmd: string): Promise<void>;
+/**
+ * Generates a script based on the history of what you typed in the REPL. Three alternatives for specifying this in the params:
+ * [int]: the total number of lines going back that you want
+ * [int, int]: the range of lines that you want (e.g. [2, 4] gets the last 4 lines except the last line you typed)
+ * [...int[]]: the indices of the lines that you wanted, counting back from the current line
+ */
+declare function scriptFromHistory(from: number): string;
 
-declare global {
-  interface Window {
-    /**
-     * Execute a command that will be executed in the underlying shell environment.
-     */
-    exec(cmd: string): Promise<void>;
-    /**
-     * Generates a script based on the history of what you typed in the REPL. Three alternatives for specifying this in the params:
-     * [int]: the total number of lines going back that you want
-     * [int, int]: the range of lines that you want (e.g. [2, 4] gets the last 4 lines except the last line you typed)
-     * [...int[]]: the indices of the lines that you wanted, counting back from the current line
-     */
-    scriptFromHistory(from: number): string;
+// For these fs-like methods, for args with the word 'path' in them you can use relative (to the cwd) or absolute paths
+// Exept for exists(), all of these have a command version as well for ease of use.
+declare function mkdir(dirPath: string): void;
+declare function exists(path: string): void;
+declare function move(filePath: string, moveToDirPath: string): void;
+declare function copy(filePath: string, copyToDirPath: string): void;
+declare function rename(filePath: string, newFileName: string): void;
+declare function metadata(filePath: string): Promise<DirentWithMetadata>;
+declare function getEnts(filePath: string, opts?: IGetEntsFilters): DirentWithMetadata[];
+declare function getEntsDeep(filePath: string, opts?: IGetEntsFilters): DirentWithMetadata[];
+declare function getEntsWithMetadata(filePath: string, opts?: IGetEntsFilters): Promise<DirentWithMetadata[]>;
+declare function getEntsNames(filePath: string, opts?: IGetEntsFilters): string[];
 
-    // For these fs-like methods, for args with the word 'path' in them you can use relative (to the cwd) or absolute paths
-    // Exept for exists(), all of these have a command version as well for ease of use.
-    mkdir(dirPath: string): void;
-    exists(path: string): void;
-    move(filePath: string, moveToDirPath: string): void;
-    copy(filePath: string, copyToDirPath: string): void;
-    rename(filePath: string, newFileName: string): void;
-    metadata(filePath: string): Promise<DirentWithMetadata>;
-    getEnts(filePath: string, opts?: IGetEntsFilters): DirentWithMetadata[];
-    getEntsDeep(filePath: string, opts?: IGetEntsFilters): DirentWithMetadata[];
-    getEntsWithMetadata(filePath: string, opts?: IGetEntsFilters): Promise<DirentWithMetadata[]>;
-    getEntsNames(filePath: string, opts?: IGetEntsFilters): string[];
+declare function doForEach(filePath: string, callback: (ent: DirentWithMetadata, folder: string) => void, opts?: IGetEntsFilters & IScanOptions): Promise<void>;
+declare function doForEachDeep(filePath: string, callback: (ent: DirentWithMetadata, folder: string) => void, opts?: IGetEntsFilters & IScanOptions): Promise<void>;
 
-    doForEach(filePath: string, callback: (ent: DirentWithMetadata, folder: string) => void, opts?: IGetEntsFilters & IScanOptions): Promise<void>;
-    doForEachDeep(filePath: string, callback: (ent: DirentWithMetadata, folder: string) => void, opts?: IGetEntsFilters & IScanOptions): Promise<void>;
+// The manual version of resolving a relative/absolute path
+declare function resolvePath(filePath: string): string;
 
-    // The manual version of resolving a relative/absolute path
-    resolvePath(filePath: string): string;
+// Set exif metadata
+declare function setTags(filePath: string, tags: Record<string, any>): Promise<void>;
 
-    // Set exif metadata
-    setTags(filePath: string, tags: Record<string, any>): Promise<void>;
+declare const env: ENV; // Used for some internal stuff, but also contains e.g. cwd
+declare const JSONbig: JSONBigInt;
 
-    env: ENV; // Used for some internal stuff, but also contains e.g. cwd
-    JSONbig: JSONBigInt;
+declare const lo: Lodash;
 
-    lo: Lodash;
+// todo: write docs for these functions
 
-    // todo: write docs for these functions
+declare function readJson(filePath: string): any;
+declare function readFile(filePath: string, encoding?: string): string;
+declare function writeJson(filePath: string, data: any, log?: boolean): void;
+declare function writeFile(filePath: string, data: any, opts?: { log?: boolean, encoding?: string }): void;
+declare function editFile(filePath: string, editCallback: (fileContents: string) => string): void;
 
-    readJson(filePath: string): any;
-    readFile(filePath: string, encoding?: string): string;
-    writeJson(filePath: string, data: any, log?: boolean): void;
-    writeFile(filePath: string, data: any, opts?: { log?: boolean, encoding?: string }): void;
-    editFile(filePath: string, editCallback: (fileContents: string) => string): void;
+// setTags: wrapResolvePath1(setExifMetadata);
 
-    // setTags: wrapResolvePath1(setExifMetadata);
+declare function esc(str: string): string;
+declare function escPath(str: string): string;
+declare function cwdRel(path: string): string;
 
-    esc(str: string): string;
-    escPath(str: string): string;
-    cwdRel(path: string): string;
+declare function copyClb(str: string): void;
+declare function copyClbJSON(entity: any): void;
+declare function pasteClb(): string;
+declare function pasteClbJS(): void;
+declare function pasteClbJSON(): any;
+declare function editClb(editCallback: (oldClb: string) => string): void;
 
-    copyClb(str: string): void;
-    copyClbJSON(entity: any): void;
-    pasteClb(): string;
-    pasteClbJS(): void;
-    pasteClbJSON(): any;
-    editClb(editCallback: (oldClb: string) => string): void;
+declare function setConsoleIndent(indents: number): void;
+declare function highlightExp(strings: TemplateStringsArray, ...exps: (string | number)[]): string;
+declare function listlan(): string[]; // list languages
 
-    setConsoleIndent(indents: number): void;
-    highlightExp(strings: TemplateStringsArray, ...exps: (string | number)[]): string;
-    listlan(): string[], // list languages
+declare function splitFileName(fileName: string, isDirectory?: boolean): [nameBase: string, ext: string];
 
-    splitFileName(fileName: string, isDirectory?: boolean): [nameBase: string, ext: string];
-
-    question(q: string): Promise<string>;
-  }
-}
+declare function question(q: string): Promise<string>;
 
 // todo: tell about r`oijwe\foi`
